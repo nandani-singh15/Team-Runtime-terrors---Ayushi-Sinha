17e7e62af2f8cedf583146691d61d09dfeb2bbda
Nandani Singh <nandani121singh@gmail.com>
2026-07-29 09:30:00 +0530

    chore: initialize project workspace and adjust pom.xml dependencies

--- src/main/java/com/swasuraksha/SwasurakshaBackendApplication.java
+++ src/main/java/com/swasuraksha/SwasurakshaBackendApplication.java
@@ -2,8 +2,10 @@ package com.swasuraksha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SwasurakshaBackendApplication {
 
 	public static void main(String[] args) {
