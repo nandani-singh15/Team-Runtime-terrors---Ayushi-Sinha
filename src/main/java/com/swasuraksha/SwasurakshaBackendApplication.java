b488587ae1338913d12e85a3d4de6ed8794224f1
Nandani Singh <nandani121singh@gmail.com>
2026-07-30 16:08:00 +0530

    feat: initialize Spring Boot backend project

---
+++ src/main/java/com/swasuraksha/SwasurakshaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swasuraksha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwasurakshaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwasurakshaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swasuraksha/SwasurakshaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swasuraksha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwasurakshaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
