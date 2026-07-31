28960e43e7501c6961fce72e7889cc543e931f1f
Nandani Singh <nandani121singh@gmail.com>
2026-07-31 12:39:40 +0530

    min config change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8086/api/v1',
+  baseURL: 'http://localhost:8088/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
